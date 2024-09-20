1f1eea94d48178d1c56b15f490fa2232525ee3a0
vo1d-null <vo1d.null299@gmail.com>
2024-09-20 21:01:32 +0300

    basic nav added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,13 @@
-import localFont from "next/font/local";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+//components
+import Header from "@/components/Header";
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets : ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: '--font-jetbrainsMono',
 });
 
 export const metadata = {
@@ -20,11 +18,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={jetbrainsMono.variable}>
+        <Header />
+        {children}</body>
     </html>
   );
 }
