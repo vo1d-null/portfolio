3079a17da8d6707914f78b1dd6762ab98492dc8d
vo1d-null <vo1d.null299@gmail.com>
2024-10-04 18:57:07 +0300

    stairs animation logic framer motion

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { AnimatePresence } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import { usePathname } from "next/navigation"
+import Stairs from "./Stairs";
 
 
 
@@ -15,6 +16,13 @@ const StairTransition = () => {
                 pointer-events-none z-40 flex">
                     <Stairs />
                 </div>
+                <motion.div className="h-screen w-screen fixed bg-primary top-0
+                pointer-events-none"
+                initial={{opacity: 1}}
+                animate={{
+                  opacity: 0,
+                  transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+                  }} />
             </div>
         </AnimatePresence>
         ;
--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -13,6 +13,10 @@ const stairAnimation = {
     },
 }
 
+const reverseIndex = (index) => {
+  const totalSteps = 6;
+  return totalSteps - index - 1;
+}
 
 const Stairs = () => {
   return <>
@@ -21,7 +25,23 @@ const Stairs = () => {
     The delay for each div is calculated dynamically based on its reversed index,
     creating a staggering effect with decreasing delay for each subsequent step.
     */}
-
+        {[...Array(6)].map((_, index)=> {
+          return (
+              <motion.div 
+                key={index} 
+                variants={stairAnimation} 
+                initial="initial"
+                animate="animate" 
+                exit="exit" 
+                transition={{
+                  duration: 0.4,
+                  ease: "easeInOut",
+                  delay: reverseIndex(index) * 0.1,
+                }}
+              className= "h-full w-full bg-white relative"
+            />
+          );
+    })}
 
   </>
 }
