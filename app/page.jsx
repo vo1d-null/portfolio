e1e9a8ea1f9e50e12408f762f3543e1891ffd873
vo1d-null <vo1d.null299@gmail.com>
2024-10-06 20:51:51 +0300

    btn cv

--- app/page.jsx
+++ app/page.jsx
@@ -8,10 +8,16 @@ const Home = () => {
           {/*text*/}
           <div className="text-center xl:text-left">
             <span className="text-xl"> Junior Fullstack DevOps Engineer </span>
-            <h1 className="h1">
+            <h1 className="h1 mb-6">
               Hello Im <br /> <span className="text-accent">Nikolay Gerginov</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">As a Junior Fullstack DevOps Engineer, I build and improve features using Python and JavaScript, set up CI/CD pipelines, and manage infrastructure with Terraform. I also deploy apps with Docker and Kubernetes while keeping an eye on performance.</p>
+            {/* btn and socials */}
+            <div>
+              <Button>
+                <span>Download CV</span>
+              </Button>
+            </div>
           </div>
           {/*photo*/}
           <div>photo</div>
