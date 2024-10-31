aa1156f880fe2acc418b7f1141e9bbf2e5c8e520
vo1d-null <vo1d.null299@gmail.com>
2024-10-31 20:30:30 +0200

    work page part 2 , contact page,input select and textarea customization

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -69,7 +69,7 @@ const Work = () => {
     initial={{opacity: 0}} 
     animate={{
       opacity: 1, 
-      transition:{ delay: 2.4, duration: 0.4, ease: "easeIn" } }}
+      transition:{ delay: 2.4, duration: 0.4, ease: "easeIn" }, }}
     className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0"
     >
       <div className="container mx-auto">
